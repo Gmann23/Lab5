290fa80797bf79720269d2cfa479e51db95a1f10
Gmann23 <Gmann23@git.com>
2021-10-17 19:21:15 -0700

    completed part 2

---
+++ turnin/gmann007_lab5_part2.c
@@ -0,0 +1,118 @@
+/*	Author: Gurvinder Mann
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #5  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+
+
+enum States { Start, Init, Wait, Dec, Inc, Reset } state;
+
+
+int main(void) {
+	/* Insert DDR and PORT initializations */
+	/* Insert your solution below */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x07;
+	state = Start;
+	while (1) {
+		Tick();
+	}
+	return 1;
+}
+void Tick() {
+	switch (state) {
+	case Start: {
+		state = Init;
+		break;
+	}
+
+	case Init:
+		if (PINA == 0x01) {
+			state = Inc; break;
+		}
+		else if (PINA == 0x02) {
+			state = Dec; break;
+		}
+		else if (PINA == 0x03) {
+			state = Reset; break;
+		}
+		else if (PINA == 0x00) {
+			state = Init; break;
+		}
+		break;
+	case Wait:
+		if ((PINA == 0x01) || (PINA == 0x02)) {
+			state = Wait; break;
+		}
+		else if ((PINA & 0x03) == 0x03) {
+			state = Reset; break;
+		}
+		else {
+			state = Init; break;
+		}
+
+	case Inc:
+		state = Wait;
+		break;
+
+	case Dec:
+		state = Wait;
+		break;
+
+
+	case Reset:
+		if ((PINA == 0x01) && (PINA == 0x02)) {
+			state = Reset; break;
+		}
+		else if (PINA == 0x00) {
+			state = Init; break;
+		}
+
+	default:
+		break;
+	}
+	switch (state) {
+	case Start: {
+		PORTC = 0x07;
+	}
+			  break;
+	case Wait:
+	case Init:
+		break;
+
+	case Inc: {
+		if (PORTC >= 0x09) {
+			PORTC = 0x09; break;
+		}
+		else {
+			PORTC = PORTC + 0x01; break;
+		}
+	}
+			break;
+
+	case Dec: {
+		if (PORTC <= 0x00) {
+			PORTC = 0x00; break;
+		}
+		else {
+			PORTC = PORTC - 0x01; break;
+		}
+	}
+			break;
+
+
+
+	case Reset: {
+		PORTC = 0x00; break;
+	}
+	}
+}
